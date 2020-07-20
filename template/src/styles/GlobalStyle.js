df2d417b846f7a4e82b1ce1e947fdf57aa9c8983
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-07-19 22:29:34 -0300

    Changed global styles

--- template/src/styles/GlobalStyle.js
+++ template/src/styles/GlobalStyle.js
@@ -7,11 +7,13 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
   body { 
-    overflow: hidden;
     background-color: ${(props) => props.theme.background}
   }
   a {
     text-decoration: none
+  }
+  ul {
+    list-style: none;
   }
 `;
 
